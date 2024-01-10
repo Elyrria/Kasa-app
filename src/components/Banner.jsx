1d1737e59aca0cf700eec79e7b6c531c4e856ea7
Quentin Dumon <quentindumon352a@gmail.com>
2024-01-10 19:02:44 +0100

    Validation du composant Banner et du style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,37 @@
+import bannerHome1240 from "../assets/banner-home-1240.webp"
+import bannerHome375 from "../assets/banner-home-375.webp"
+import bannerAbout1240 from "../assets/banner-about-1240.webp"
+import bannerAbout375 from "../assets/banner-about-375.webp"
 import "../styles/Banner.scss"
+
+const sizeBanner = [1240, 375]
 
 function Banner({ activeBanner }) {
     console.log(activeBanner)
     return (
-        //? Ajouter une props pour modifier la class active-**-** (Permettra de modifier l'image en background en fontion )
-        <div className={`bannerWrapper ${activeBanner}`}>
+        <div className="bannerWrapper">
             <div className="bannerWrapper--opacity"></div>
-
+            {/* //! Condition qui permet de vérifier si le texte doit être affiché  */}
             {activeBanner === "active-home" && (
                 <p className="bannerWrapper__para">
                     Chez vous, partout et ailleurs
                 </p>
+            )}
+            {/* //! Condition qui permet de vérifier quelle image afficher dans la banière  */}
+            {activeBanner === "active-home" ? (
+                <img className="scaleHome"
+                    src={bannerHome1240}
+                    srcSet={` ${bannerHome375} ${sizeBanner[1]}w, ${bannerHome1240} ${sizeBanner[0]}w  `}
+                    sizes={`(max-width: ${sizeBanner[1]}px) ${sizeBanner[1]}px, ${sizeBanner[0]}px`}
+                    alt="Photographie de falaises avec la mer"
+                />
+            ) : (
+                <img className="scaleAbout"
+                    src={bannerAbout1240}
+                    srcSet={`${bannerAbout1240} ${sizeBanner[0]}w, ${bannerAbout375} ${sizeBanner[1]}w `}
+                    sizes={`(max-width: ${sizeBanner[1]}px) ${sizeBanner[1]}px, ${sizeBanner[0]}px`}
+                    alt="Photographie d'un fleuve avec des montagnes'"
+                />
             )}
         </div>
     )
